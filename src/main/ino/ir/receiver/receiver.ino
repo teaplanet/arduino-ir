049cb4efd9d21501d7af41e322b5673f30762243
Ken <ken+git@teapla.net>
2012-08-29 01:02:16 +0900

    rewrite function of receive.
    added message when to use serial.

--- src/main/ino/ir/receiver/receiver.ino
+++ src/main/ino/ir/receiver/receiver.ino
@@ -18,58 +18,75 @@ void setup() {
 }
 
 void loop() {
-	byte c = serialValue();
-	if (c == 'r') {
-		unsigned int data[MAX_RECV_SIZE] = {};
-		int size = receive(data);
-		Serial.print("data size: ");
-		Serial.print(size, DEC);
-		Serial.println("");
+	if (Serial.available()) {
+		byte c = Serial.read();
+		if (c == 'r') {
+			unsigned int data[MAX_RECV_SIZE] = {};
+			int size = receive(data);
+			Serial.print("[i] data size: ");
+			Serial.print(size, DEC);
+			Serial.println("");
+
+			Serial.println("----- data -----");
+			for (int i=0; i<size; ++i) {
+				Serial.print(data[i]);
+				Serial.print(", ");
+			}
+			Serial.println("");
+			Serial.println("----------------");
+		}
 	}
 }
 
 unsigned int receive(unsigned int* data) {
-	int pos = -1;
-	unsigned int in = 0;
-	unsigned int last = 1;
+	Serial.println("[i] Start to receive.");
+	Serial.println("[i] Stop enter 's'. The other key to reset the data.");
+	Serial.println("[i] Receiving...");
 
-	unsigned long micSec = 0;
-	const unsigned long max = 2000000;	// 2sec
+	// デバイス
+	byte c = 0x00;			// シリアルからのコマンド入力
+	int in = 0;				// IRの入力値
+	int last = 1;			// 前回の最終入力値
 
-	bool receiving = true;
-	Serial.println("Ready to receive.");
-	unsigned long start = micros();
-	while (serialValue() != 's') {
-//		start = micros();	// ここでstartを再計算すると計測値がとても小さくなる(4〜8ms)。
-		while ((in = digitalRead(IR_RECV_PIN)) == last) {
-			micSec = micros() - start;
-			if (micSec >= max) {
-				pos = -1;
-				Serial.println("/");
+	// 状態
+	bool receiving = true;	// 受信中
+	int pos = -1;			// データの位置
+
+	// 時間
+	unsigned long interval = 0;			// ON/OFF切り替え間隔(micro sec)
+	unsigned long start = micros();		// 開始時刻
+
+	while (receiving) {
+		in = digitalRead(IR_RECV_PIN);
+		if (in != last) {
+			unsigned long now = micros();
+			interval = now - start;
+			start = now;
+//			Serial.println(interval);
+
+			if (pos >= 0) {
+				data[pos] = interval;
+			}
+
+			if (++pos == MAX_RECV_SIZE) {
+				Serial.println("[!] Receive buffer is full.");
 				break;
 			}
+			last = in;
 		}
 
-		unsigned long now = micros();
-		micSec = now - start;
-		Serial.print(micSec, DEC);
-		Serial.print(",");
-
-		if (pos++ >= 0) {
-			data[pos] = micSec;
+		if (Serial.available()) {
+			c = Serial.read();
+			if (c == 's') {
+				receiving = false;
+			} else if ((c == 10) || (c == 13)) {
+				// do nothing.
+			} else {
+				pos = -1;
+				Serial.println("[!] Reset the received data.");
+			}
 		}
-
-		last = in;
-		start = now;
 	}
-	Serial.println("Stop to receive.");
-	return --pos;
-}
-
-byte serialValue() {
-	byte c = 0x00;
-	if (Serial.available()) {
-		c = Serial.read();
-	}
-	return c;
+	Serial.println("[i] Stop to receive.");
+	return pos;
 }
